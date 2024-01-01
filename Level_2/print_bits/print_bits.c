7c919000c281d61c5016cfe619a6e156c7a22304
Aleix <aleixkmb@gmail.com>
2024-01-01 23:00:15 +0100

    added print bits main

--- Level_2/print_bits/print_bits.c
+++ Level_2/print_bits/print_bits.c
@@ -1,11 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+
 void	print_bits(unsigned char octet)
 {
   int i = 8;
   unsigned char bit;
 
-  while (i--)
+  while (i)
   {
-    bit = (octet >> i % 1) + '0';
+    i--;
+    bit = (octet >> i) & 1;
+    bit += '0';
     write (1, &bit, 1);
   }
+}
+
+int main(void)
+{
+    print_bits(40);
+    return 0;
 }
